177fe4fee358b647b632c2cde68e3c427d8afe1b
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-06 11:35:28 +0100

    feat(lib function): add curry2, curry3, curry4

--- lib/ramdart.dart
+++ lib/ramdart.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+R Function(U) Function(T) curry2<T, U, R>(R Function(T, U) f) {
+  return (T t) => (U u) => f(t, u);
+}
+
+R Function(V) Function(U) Function(T) curry3<T, U, V, R>(
+    R Function(T, U, V) f) {
+  return (T t) => (U u) => (V v) => f(t, u, v);
+}
+
+R Function(W) Function(V) Function(U) Function(T) curry4<T, U, V, W, R>(
+    R Function(T, U, V, W) f) {
+  return (T t) => (U u) => (V v) => (W w) => f(t, u, v, w);
 }
