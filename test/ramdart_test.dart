705d56a4da29ea980f339c38e156ca6fd1dac5a0
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-06 11:35:56 +0100

    feat(test function): test curry2, curry3, curry4

--- test/ramdart_test.dart
+++ test/ramdart_test.dart
@@ -1,8 +1,22 @@
 import 'package:ramdart/ramdart.dart';
 import 'package:test/test.dart';
 
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('function currying:', () {
+    test('(curry2)', () {
+      var curried = curry2((int a, int b) => a + b);
+      expect(curried(2)(3), 5);
+    });
+
+    test('(curry3)', () {
+      var curried = curry3((int a, int b, int c) => a + b + c);
+      expect(curried(2)(3)(4), 9);
+    });
+
+    test('(curry4)', () {
+      var curried = curry4((int a, int b, int c, int d) => a + b + c + d);
+      expect(curried(2)(3)(4)(5), 14);
+    });
   });
 }
